6b3f2b07acb846c3d3b105859f77f477a1b5b10c
Cristian Guaman <cristian.guaman@icloud.com>
2021-08-06 15:22:20 -0500

    Readme file was updated

--- src/main/java/com/example/blazetest/controllers/OrderController.java
+++ src/main/java/com/example/blazetest/controllers/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/orders")
 public class OrderController {
   @Autowired
--- src/main/java/com/example/blazetest/controllers/ProductController.java
+++ src/main/java/com/example/blazetest/controllers/ProductController.java
@@ -10,6 +10,7 @@ import com.example.blazetest.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/products")
 public class ProductController {
   @Autowired
